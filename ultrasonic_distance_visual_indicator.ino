947a74fc8c4f85c9b3d98d72a04222a54669e24e
KESHAV <k.keshavkumar1590@gmail.com>
2026-09-14 19:41:34 +0530

    Update ultrasonic_distance_visual_indicator.ino

--- ultrasonic_distance_visual_indicator.ino
+++ ultrasonic_distance_visual_indicator.ino
@@ -1,38 +1,56 @@
 int trigg=12;
 int echo=11;
 int arled[]={13,9,8,7,6,5,4,3,2};
-int totled=10;
+int totled=9;
+int button=10;
+int butt=0;
+bool holdn = false;       
+int laststate = LOW;   
+float dist = 0;              
+float time_val = 0; 
+
 void setup() {
   pinMode(trigg,OUTPUT);
   pinMode(echo,INPUT);
-  for (int i=0;i<10;i++){
+  pinMode(button,INPUT);
+
+  for (int i=0;i<totled;i++){
     pinMode(arled[i],OUTPUT);
   }
   Serial.begin(9600);
 }
  // put your setup code here, to run once:
 void loop() {
-digitalWrite(trigg,LOW);
-delayMicroseconds(10);
-digitalWrite(trigg,HIGH);
-delayMicroseconds(10);
-digitalWrite(trigg,LOW);
-int ledsglow=0;
+  int currstate=digitalRead(button);
+  if (currstate == HIGH && laststate==LOW){
+    holdn = !holdn;
+    delay(50); 
+  }
+  laststate=currstate;
 
-float time=pulseIn(echo,HIGH);
-float dist=(time*0.034)/2;
-if(dist>0 && dist<=100){
-  ledsglow=totled-dist/10;
-  if(ledsglow <= 0){
-    ledsglow=1;
+  if (!holdn){
+    digitalWrite(trigg,LOW);
+    delayMicroseconds(10);
+    digitalWrite(trigg,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigg,LOW);
 
+    time_val = pulseIn(echo,HIGH);
+    dist = (time_val * 0.034) / 2;
   }
 
-}
-else if(dist>100 || dist==0){
-  ledsglow=0;
-}
-for (int i = 0; i < totled; i++) {
+  int ledsglow=0;
+  if(dist>0 && dist<=100){
+    ledsglow = totled - (int)(dist/(100/totled)); 
+    if(ledsglow <= 0){
+      ledsglow=1;
+    }
+  }
+  else if(dist>100 || dist==0){
+    ledsglow=0;
+  }
+
+  for (int i = 0; i < totled; i++) {
     if (i < ledsglow) {
       digitalWrite(arled[i], HIGH);
     } else {
@@ -40,10 +58,11 @@ for (int i = 0; i < totled; i++) {
     }
   }
 
-Serial.print("distance: ");
-Serial.print(dist);
-Serial.print(" cm;\n");
-
-
-delay(500);
+  if (holdn){
+    Serial.print("[HELD] "); 
+  }
+  Serial.print("distance: ");
+  Serial.print(dist);
+  Serial.print(" cm;\n");
+  delay(500);
 }
